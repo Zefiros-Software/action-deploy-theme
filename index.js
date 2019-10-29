c8a34e5de6fa17a09da69a40a7c8b11401d6ee9a
Paul Visscher <p.e.visscher@zefiros.io>
2019-10-29 18:30:04 +0100

    chore: correct base path

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ const GhostAdminApi = require('@tryghost/admin-api');
             key: core.getInput('api-key'),
             version: 'canary'
         });
-
-        const basePath = process.env.GITHUB_WORKSPACE;
-        const pkgPath = path.join(process.env.GITHUB_WORKSPACE, 'package.json');
+        const source = core.getInput('source')
+        const basePath = path.join(process.env.GITHUB_WORKSPACE, source);
+        const pkgPath = path.join(basePath, 'package.json');
         const themeName = core.getInput('theme-name') || require(pkgPath).name;
         const themeZip = `${themeName}.zip`;
         const zipPath = path.join(basePath, themeZip);
         const exclude = core.getInput('exclude') || '';
 
         // Create a zip
-        await exec.exec(`zip -r ${themeZip} ${core.getInput('source')} -x *.git* *.zip yarn* npm* *routes.yaml *redirects.yaml *redirects.json ${exclude}`, [], {cwd: basePath});
+        await exec.exec(`zip -r ${themeZip} . -x *.git* *.zip yarn* npm* *routes.yaml *redirects.yaml *redirects.json ${exclude}`, [], {cwd: basePath});
 
         // Deploy it to the configured site
         await api.themes.upload({file: zipPath});
